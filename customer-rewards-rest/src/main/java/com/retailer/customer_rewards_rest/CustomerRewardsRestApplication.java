96bf00d09ac546b6d8ba6c243a81d714992b75d8
Kotesh Chintha <koteshchintha023@gmail.com>
2025-06-09 23:16:37 -0500

    Initial commit

---
+++ customer-rewards-rest/src/main/java/com/retailer/customer_rewards_rest/CustomerRewardsRestApplication.java
@@ -0,0 +1,13 @@
+package com.retailer.customer_rewards_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRewardsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRewardsRestApplication.class, args);
+	}
+
+}
---
+++ customer-rewards-rest/src/test/java/com/retailer/customer_rewards_rest/CustomerRewardsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailer.customer_rewards_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRewardsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
