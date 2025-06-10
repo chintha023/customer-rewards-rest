f254b1f1ce0b4386581d0fea8440b981ad15eb88
Kotesh Chintha <koteshchintha023@gmail.com>
2025-06-09 23:42:45 -0500

    Inital commit from intellij

--- customer-rewards-rest/src/main/java/com/retailer/offers/CustomerRewardsRestApplication.java
+++ customer-rewards-rest/src/main/java/com/retailer/offers/CustomerRewardsRestApplication.java
@@ -1,4 +1,4 @@
-package com.retailer.customer_rewards_rest;
+package com.retailer.offers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
